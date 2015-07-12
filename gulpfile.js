374d533fa93896beef759e0b76aa52e968a2b110
Thomas Hourlier <thomas.hourlier@cnode.fr>
2015-07-11 19:32:28 -0700

    Added posibility to create whether a circle or a hexagon.
    Renaming of the component to wid-tile.

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ var tag_version = require('gulp-tag-version');
 var filter = require('gulp-filter');
 
 var POLYSERVE_PORT = 8080,
-    elementName = 'wid-hex-tile';
+    elementName = 'wid-tile';
 
 var browserSyncConfig = function(path, cb) {
   bs.init({
